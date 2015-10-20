428d216ea849080a6681506eda127e939e703ff6
andreacastillo <andrea_Castillo@knights.ucf.edu>
2015-10-20 18:29:54 -0400

    close but not good

--- planner.pl
+++ planner.pl
@@ -11,27 +11,18 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 :- module( planner,
 	   [
-	       plan/5, change_state/3,conditions_met/2,member_state/2,
-	       move/3,go/2,test/0,test2/0
+	       plan/5,change_state/3,conditions_met/2,member_state/2,
+	       move/3,go/2,test/0,test2/0, test1/0
 	   ]).
 
 :- [utils].
 
-plan(State, Goal, _, Moves,  _) :-	equal_set(State, Goal),
-				write('moves are'), nl,
-				reverse_print_stack(Moves).
+plan(State, Goal, _, Moves, 0) :-
+   equal_set(State, Goal),
+	write('moves are'), nl,
+	reverse_print_stack(Moves).
 
-plan(State,Goal,Been_list, Moves, Level) :-
-   New_Level is Level+1,
-  plan(State, Goal, Been_list, Moves, New_Level).
-
-plan(State,Goal,Been_list, Moves, Level):-
-   planDB(State, Goal, Been_list, Moves, Level).
-
-
-
-
-planDB(State, Goal, Been_list, Moves, Level) :-
+bfs(State, Goal, Been_list, Moves, Level) :-
 				Level > 0,
 				move(Name, Preconditions, Actions),
 				conditions_met(Preconditions, State),
@@ -39,8 +30,15 @@ planDB(State, Goal, Been_list, Moves, Level) :-
 				not(member_state(Child_state, Been_list)),
 				stack(Child_state, Been_list, New_been_list),
 				stack(Name, Moves, New_moves),
-				New_Level is Level-1,
-			planDB(Child_state, Goal, New_been_list, New_moves, New_Level),!.
+				New_Level is Level -1,
+			bfs(Child_state, Goal, New_been_list, New_moves,  New_Level),!.
+
+plan(State, Goal, Been_list, Moves, Level) :-
+   New_Level is Level+1,
+   bfs_call(State, Goal, Been_list, Moves, New_Level).
+ 
+ bfs_call(State, Goal, Been_list, Moves, Level) :-
+ 		bfs(State, Goal, Been_list, Moves, Level).
 
 change_state(S, [], S).
 change_state(S, [add(P)|T], S_new) :-	change_state(S, T, S2),
@@ -63,11 +61,11 @@ move(pickup20(X), [handempty, clear2(X), room(2), on2(X, Y)],
 		[del(handempty), del(clear2(X)), del(on2(X, Y)),
 				  add(clear2(Y)),	add(holding(X))]).
 
-move(pickup(X), [handempty, room(1),  room(1), clear(X), ontable(X)],
+move(pickup(X), [handempty, room(1),  clear(X), ontable(X)],
 		[del(handempty), del(clear(X)), del(ontable(X)),
 				 add(holding(X))]).
 
-move(pickup2(X), [handempty, room(2), room(2), clear2(X), ontable2(X)],
+move(pickup2(X), [handempty, room(2), clear2(X), ontable2(X)],
 		[del(handempty), del(clear2(X)), del(ontable2(X)),
 				 add(room(2)),add(holding(X))]).
 
@@ -100,6 +98,9 @@ go(S, G) :- plan(S, G, [S], [], 0).
 
 test :- go([handempty, room(1), ontable(b), on(a,b),  clear(a)],
 	          [handempty,  room(2), ontable2(b), on2(a,b), clear2(a)]).
+
+test1 :- go([handempty, room(1), ontable(b), on(c,b), ontable2(a) , clear(c), clear2(a)],
+	          [handempty,  room(2), ontable2(c), on2(a,b), on2(b,c), clear2(a)]).
 
 test2 :- go([handempty, room(1), ontable(a), clear(a)],
 	          [handempty, room(1), ontable2(a), clear2(a)]).
